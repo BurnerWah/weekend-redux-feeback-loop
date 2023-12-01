f6cfbd50547e1f7217e11442bbd76ceee484bc4e
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-01 15:01:49 -0600

    Formatting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/App/App'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// @ts-ignore
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
