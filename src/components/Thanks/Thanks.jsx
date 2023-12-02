9af6e0c2bfd054858e9fbb81e235f599e9b0bd52
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-02 11:57:12 -0600

    Made thanks page go back to feeling page

--- src/components/Thanks/Thanks.jsx
+++ src/components/Thanks/Thanks.jsx
@@ -1,8 +1,13 @@
+import { useHistory } from 'react-router-dom'
+
 export default function Thanks() {
+  const history = useHistory()
   return (
     <div>
       <h2>Thank You!</h2>
-      <button data-testid="next">Leave New Feedback</button>
+      <button onClick={() => history.push('/feeling')} data-testid="next">
+        Leave New Feedback
+      </button>
     </div>
   )
 }
