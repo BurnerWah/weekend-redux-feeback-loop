39e0bd4b8c420cf7bd650d47aeda2ae1662ff93d
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-02 11:08:42 -0600

    Added router and feeling step component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,25 @@
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom'
+import FeelingStep from '../FeelingStep/FeelingStep'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+        <Switch>
+          <Route path="/feeling">
+            <FeelingStep />
+          </Route>
+          <Route path="/">
+            <Redirect to="/feeling" />
+          </Route>
+        </Switch>
+      </div>
+    </HashRouter>
   )
 }
 
---
+++ src/components/FeelingStep/FeelingStep.jsx
@@ -0,0 +1,9 @@
+export default function FeelingStep() {
+  return (
+    <div>
+      <h2>How are you feeling today?</h2>
+      <input type="number" placeholder="1-5" />
+      <button>Next</button>
+    </div>
+  )
+}
