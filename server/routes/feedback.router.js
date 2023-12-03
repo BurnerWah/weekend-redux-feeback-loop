baa37f62ee0f15ebfe747d40ec75f93095b5a536
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-03 12:29:57 -0600

    Formatting - feedback router

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const router = express.Router();
+const { Router } = require('express')
 const pool = require('../modules/pool')
 
-
+const router = Router()
 
 // TODO: This route adds a new feedback entry
-router.post('/', (req, res) => {
-
-})
-
+router.post('/', (req, res) => {})
 
 // DO NOT EDIT THIS ROUTE
 // This route must return all feedback.
 router.get('/', (req, res) => {
-    console.log('testing')
-    const sqlText = `SELECT * FROM "feedback" ORDER BY "id"`;
-    pool.query(sqlText).then(result => {
-        res.send(result.rows)
-    }).catch(err => {
-        console.log(err);
-        res.sendStatus(500);
+  console.log('testing')
+  const sqlText = `SELECT * FROM "feedback" ORDER BY "id"`
+  pool
+    .query(sqlText)
+    .then((result) => {
+      res.send(result.rows)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.sendStatus(500)
     })
 })
 
-module.exports = router;
+module.exports = router
